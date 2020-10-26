fd4e075a701dd23b87cbbea18e5000f7ca1dc19d
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-26 12:35:00 -0500

    WishList - mapping over wishlist

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.scss';
 import Nav from './Components/Nav/Nav';
 import Quote from './Components/Quote/Quote'
 
+
+// DELETE
+import WishList from './Components/WishList/WishList'
+
 function App() {
 	return (
 		<div className='App'>
@@ -11,6 +15,9 @@ function App() {
 				<h2>Resources to pass the time & to take care of your mind</h2>
 				<Quote />
 			</main>
+			
+		{/* DELETE: */}
+			<WishList />
 		</div>
 	);
 }
--- src/Components/WishList/WishList.js
+++ src/Components/WishList/WishList.js
@@ -1,6 +1,61 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './WishList.scss';
 
-export default function WishList() {
-	return <div>WishList</div>;
-}
+
+export default function WishList(props) {
+
+// FOR TESTING////////////
+//REMOVE COMPONENT FROM APP.JS!!!
+	const wishList = [{
+		title: "walk",
+		time_minutes: 15,
+		category: "exercise"
+	},
+	{
+		title: "meditate",
+		time_minutes: 15,
+		category: "relax"
+	}]
+/////////////////////////////
+
+	const wishlist = wishList.map((item, index) => {
+	// const wishlist = props.wishList.map((item, index) => {
+		return(
+			<>
+			
+			<div className='list' key={index}>
+				<div className='item-info'>
+					<p className='title'>{item.title}</p>
+					<p className='time'>{item.time_minutes}</p>
+					<p className='category'>{item.category}</p>
+				</div>
+				{/* <div className='item-btns'>
+					<Link to='/edit'>
+						<button className='item-btns edit' onClick={() => {props.handleSave(item)}}>Edit</button>
+					</Link>
+					<Link to='/delete'>
+						<button className='item-btns delete' onClick={() => {props.handleDelete(item)}}>Delete</button>
+					</Link>
+					<Link to='/completed'>
+						<button className='item-btns completed' onClick={() => {props.handleCompleted(item)}}>Completed</button>
+					</Link>
+					<Link to='/like'>
+						<button className='item-btns like' onClick={() => {props.handleLike(item)}}>Like</button>
+					</Link>
+				</div> */}
+			</div>
+			</>
+		)
+	})
+
+	const loading = 'Patient pause....'
+
+	return (
+		<>
+			<div className='page-title'>Wish List</div>
+			{/* {props.wishList.length > 0 ? loading : wishlist} */}
+			{wishList.length > 0 ? wishlist : loading}
+		</>
+	)
+	}
