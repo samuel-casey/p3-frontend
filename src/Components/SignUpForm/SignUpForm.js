a3fb23d428f83585fb27be173cc7ec87f23a2bf8
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-26 11:58:13 -0400

    created basic jsx form, handleChange functions

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUpForm.scss';
 
 export default function SignUpForm() {
-	return <div>SignUpForm</div>;
+	const [formData, setFormData] = useState();
+
+	// const handleSignUp = () => {};
+
+	const handleChange = (e) => {
+		const key = e.target.name;
+		const value = e.target.value;
+		setFormData({ ...formData, [key]: value });
+		console.log(formData);
+	};
+
+	return (
+		<div className='sign-up-form'>
+			<h2>Sign up</h2>
+			<form onSubmit={handleUserSignUp}>
+				<input
+					className='form-text-input'
+					type='email'
+					name='email'
+					placeholder='email'
+					value={signUpFormUser.email}
+					onChange={handleNewUserChange}
+				/>
+				<input
+					className='form-text-input'
+					type='password'
+					name='password'
+					placeholder='password'
+					value={signUpFormUser.password}
+					onChange={handleNewUserChange}
+				/>
+				<input
+					className='form-text-input'
+					type='password'
+					name='confirmPassword'
+					placeholder='confirm password'
+					value={signUpFormUser.confirmPassword}
+					onChange={handleNewUserChange}
+				/>
+				<input type='submit' />
+			</form>
+		</div>
+	);
 }
