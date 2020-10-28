b90424d843fdcf69faa989ad163369a7560737e8
jb0nd87 <68964150+jb0nd87@users.noreply.github.com>
2020-10-28 12:57:41 -0700

    adding fav button to quote component

--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -2,32 +2,42 @@ import React, { useEffect, useContext, useState } from 'react';
 import './Quote.scss';
 import { GlobalContext } from '../../App';
 
-export default function Quote() {
+export default function Quote(props) {
+	const { gState, setGState } = useContext(GlobalContext);
 
-const { gState, setGState } = useContext(GlobalContext);
+	const [quoteInfo, setQuoteInfo] = useState({ quote: '', author: '' });
+	const { url } = gState;
+	let author;
+	let quote;
+	const getQuote = async () => {
+		const response = await fetch(url + '/quote');
+		const quoteList = await response.json();
+		const randomNumber = Math.floor(Math.random() * quoteList.length);
+		const quoteObj = quoteList[randomNumber];
+		console.log('quoteObj', quoteObj);
+		setQuoteInfo({ quote: quoteObj.quote, author: quoteObj.author });
+	};
 
-const [quoteInfo, setQuoteInfo] = useState({quote:'', author:''})
-const {url} = gState
-let author
-let quote
-const getQuote = async () => {
-	const response = await fetch(url + '/quote');
-	const quoteList = await response.json();
-	const randomNumber = Math.floor(Math.random() * quoteList.length);
-	const quoteObj = quoteList[randomNumber]
-	console.log('quoteObj', quoteObj)
-	setQuoteInfo({ quote: quoteObj.quote, author: quoteObj.author });
-};
+	useEffect(() => {
+		getQuote();
+	}, []);
 
-useEffect(() => {getQuote()}, [])
-
-	// const quotes = quoteList.map((item, index) => {
-		return (
-			<div className='quote'>
-				<div className='quote-body'>
-					<p className='random-quote'>{quoteInfo.quote} -{quoteInfo.author}</p>
-				</div>
+	const quoteList = quoteList.map((item, index) => {
+	return (
+		<div className='quote'>
+			<div className='quote-body'>
+				<p className='random-quote'>
+					{quoteInfo.quote} -{quoteInfo.author}
+				</p>
 			</div>
-		);
-	// });
+			<button
+				className='quote-btn like'
+				onClick={() => {
+					props.handleFav(item);
+				}}>
+				Like
+			</button>
+		</div>
+	);
+	});
 }
