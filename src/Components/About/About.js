2fa13e807b3696f42f74891686ced7aa83b94d16
Q <Q@Skylars-MBP.lan1>
2020-10-28 15:26:46 -0700

    nav finished

---
+++ src/Components/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './About.scss'
+
+export default function About() {
+
+    return(
+        <>
+        <h1>About</h1>
+        <div id ='intro'>Welcome to the *insert name here* App. Constructed by yours truly
+        With our App our goal is to bring a better sense of self through motivation,
+        goal structure,and help improving organisational skills.</div>
+        <div id ='Q'>about me</div>
+        <div id ='Sam'>about sam</div>
+        <div id ='Jamie'>about jaime</div>
+        <div id ='Kim'>about kim</div>
+        </>
+    )
+}
--- src/Components/TestNav/TestNav.js
+++ src/Components/TestNav/TestNav.js
@@ -1,58 +1,95 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom'
-import LogOutBtn from '../LogOutBtn/LogOutBtn';
-import { GlobalContext } from '../../App';
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import LogOutBtn from "../LogOutBtn/LogOutBtn";
+import { GlobalContext } from "../../App";
+import "../Nav/Nav.scss";
+import Nav from "react-bootstrap/Nav";
+// import Navbar from 'react-bootstrap/Navbar'
+import { initial } from "lodash";
+import { Container, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function TestNav() {
-    const { gState, setGState } = useContext(GlobalContext);
-    // const LogOutBtn ={LogOutBtn}
+  const { gState, setGState } = useContext(GlobalContext);
+  // const LogOutBtn ={LogOutBtn}
 
+  let loggedInNav = (
+    <nav>
+      <h6>Hello {gState.email}</h6>
 
+      <Link className="Links" to="/">
+        <h4>Home</h4>
+      </Link>
+      <Link className="Links" to="/favquotes">
+        <h4>Quotes</h4>
+      </Link>
+      <Link className="Links" to="/wishlist">
+        <h4>Wish List</h4>
+      </Link>
+      <Link className="Links" to="/completedlist">
+        <h4>Completed List</h4>
+      </Link>
+      <div id="hamburger">☰</div>
+      <LogOutBtn />
+    </nav>
+  );
 
-	let loggedInNav = (<nav>
-		<h6>Hello {gState.email}</h6>
-				<h6>{navLog}</h6>
-				   <Link className='Links' to='/'>
-					 <h4>Home</h4>
-				   </Link>
-				   <Link className='Links' to='/favquotes'>
-					 <h4>Quotes</h4>
-				   </Link>
-				   <Link className='Links' to='/wishlist'>
-					 <h4>Wish List</h4>
-				   </Link>
-				   <Link className='Links' to='/completedlist'>
-					 <h4>Completed List</h4>
-				   </Link>
-				   <div id="hamburger"><h3>☰</h3></div>
-				<LogOutBtn/>
-			</nav>)
+  let loggedOutNav = (
+    // <>
+    // <Nav></Nav>
+    // <Navbar>hi test</Navbar>
+	
+	
+    <Navbar collapseOnSelect expand="md" bg="" variant="white">
+      <Navbar.Brand href="#home">☰</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+		<h3 >Hello!</h3> <Link id='/.' to='/signup'>Sign Up Here!</Link>
 
-let loggedOutNav = (<nav>
-	<h6>Hello Welcome to our App</h6>
-			<Link classname= "Links" to ='/signup'>
-				<p>Click here to sign up or login</p>
-				</Link>
-			   <Link className='Links' to='/'>
-				 <h4>Home</h4>
-			   </Link>
-			   <Link className='Links' to='/favquotes'>
-				 <h4>Quotes</h4>
-			   </Link>
-			   <Link className='Links' to='/wishlist'>
-				 <h4>Wish List</h4>
-			   </Link>
-			   <Link className='Links' to='/completedlist'>
-				 <h4>Completed List</h4>
-			   </Link>
-			   <div id="hamburger"><h3>☰</h3></div>
+          <Nav.Link className="Links" href="/">Home</Nav.Link>
+		  {/* <Nav.Link className="Links" href="/signup">Sign up</Nav.Link> */}
+		  <Nav.Link className="Links" href="../about">About</Nav.Link>
+          <Nav.Link className="Links" href="/favquotes">Favorite Quotes</Nav.Link>
+		  <Nav.Link className="Links" href="/wishlist">Wish List</Nav.Link>
+		  <Nav.Link className="Links" href="/completedlist">Completed List</Nav.Link>
+		  <Nav.Link className="Links" href="/likeditems">Liked items</Nav.Link>
+          {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item>
+              <Link className="Links" to="/">
+                Home
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              {" "}
+              <Link className="Links" to="/favquotes">
+                Quotes
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              {" "}
+              <Link className="Links" to="/wishlist">
+                Wish List
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              {" "}
+              <Link className="Links" to="/completedlist">
+                Completed List
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              {" "}
+              <Link className="Links" to="/likeditems">
+                Likes
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+	
+	
+  );
 
-		</nav>)
-
-
-
-
-
-	return gState.token ? loggedInNav : loggedOutNav;
+  return gState.token ? loggedInNav : loggedOutNav;
 }
