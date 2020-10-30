ca34cd8f02a7c158849dce125e9d4059e64ecb11
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-30 11:49:18 -0400

    added GH links to about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,19 +5,21 @@ export default function About() {
 	return (
 		<>
 			<div className='about-page'>
-				<h1 id='about'>About</h1>
-				<div id='intro'>
-					Welcome to the *insert name here* App. Constructed by yours truly With
-					our App our goal is to bring a better sense of self through
-					motivation, goal structure,and help improving organisational skills.
-				</div>
+				<h1 id='about'>About pause.app</h1>
+				<div id='intro'>pause.app was built as part of a General Assembly</div>
 				<br />
+				<h1 id='about'>Team</h1>
 				<section className='custom-container'>
 					<div id='Sam' className='us'>
 						<div className='pics'>
-							<img src='https://images.unsplash.com/photo-1490718720478-364a07a997cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' />
+							<img src='https://res.cloudinary.com/scimgcloud/image/upload/v1600435704/headshot-ideo.jpg' />
 						</div>
-						<span className='abouts'>Sam</span>
+						<span className='abouts'>
+							Sam{' '}
+							<a href='https://github.com/samuel-casey/' target='blank'>
+								<i className='fab fa-github'></i>
+							</a>
+						</span>
 						<p>
 							Sam is a full-stack web developer from Boston, MA with a passion
 							for using his tech skills to help people solve problems in their
@@ -29,23 +31,38 @@ export default function About() {
 					</div>
 					<div id='Jaime' className='us'>
 						<div className='pics'>
-							<img src='https://images.unsplash.com/photo-1501673618753-48ce9418287b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' />
+							<img src='https://res.cloudinary.com/dv01780vo/image/upload/v1604070046/received_2893661924081693_2_ysiho9.jpg' />
 						</div>
-						<span className='abouts'>Jaime</span>
+						<span className='abouts'>
+							Jaime{' '}
+							<a href='https://github.com/jb0nd87 ' target='blank'>
+								<i className='fab fa-github'></i>
+							</a>
+						</span>
 						<p>content here</p>
 					</div>
 					<div id='Kim' className='us'>
 						<div className='pics'>
 							<img src='https://images.unsplash.com/photo-1586490915014-b2490ffd4727?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60' />
 						</div>{' '}
-						<span className='abouts'>Kim</span>
+						<span className='abouts'>
+							Kim{' '}
+							<a href='https://github.com/kimrass14/' target='blank'>
+								<i className='fab fa-github'></i>
+							</a>
+						</span>
 						<p>content here</p>
 					</div>
 					<div id='Q' className='us'>
 						<div className='pics'>
 							<img src='https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60' />
 						</div>
-						<span className='abouts'>Q</span>
+						<span className='abouts'>
+							Q{' '}
+							<a href='https://github.com/theletterQ1' target='blank'>
+								<i className='fab fa-github'></i>
+							</a>
+						</span>
 						<p>content here</p>
 					</div>
 				</section>
