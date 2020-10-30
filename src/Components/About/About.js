ccf661ea5285f4365c2dd19414047576589672f4
Q <Q@Skylars-MacBook-Pro.local>
2020-10-29 20:49:22 -0700

    about still needs work

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,17 +9,25 @@ export default function About() {
         <div id ='intro'>Welcome to the *insert name here* App. Constructed by yours truly
         With our App our goal is to bring a better sense of self through motivation,
         goal structure,and help improving organisational skills.</div>
-        
+
         <br/>
+
         <div className='section'>section</div>
-        <div id ='Sam' className='us'>about sam<div className='pics'></div></div>
+        <section className ='custom-container'>
+        <div className='grid'>
+        <div id ='Sam' className='us'>content here<span className='abouts'>about sam</span><div className='pics'></div></div>
+        //for sepertion
+        <div id ='Jaime' className='us'><div className='pics'></div><span className='abouts'>about jaime</span>content here</div>
+        </div>
         <br/>
-        <div id ='Jaime' className='us'><div className='pics'></div>about jaime</div>
-        <br/>
-        <div id ='Kim' className='us'>about kim<div className='pics'></div></div>
-        <br/>
-        <div id ='Q' className='us'><div className='pics'><img src='https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'/></div>about Q</div>
+        <div className ='grid'>
+        <div id ='Kim' className='us'>content here<span className='abouts'>about kim</span><div className='pics'></div> </div>
+        //may have to base examples off mine once images added
+        <div id ='Q' className='us'><div className='pics'><img src='https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'/></div><span className ='abouts'>about Q</span>content here</div>
+        </div>
+        <div className ='quote'>Quote of the Day</div>
+        </section>
         </>
-       
+
     )
 }
